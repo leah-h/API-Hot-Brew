7dca55b4ac4cc6cf39ee67b56a3b01bdc4a8c443
Leah H <harrichl@gmail.com>
2020-02-18 21:37:14 -0500

    add initial file set-up

---
+++ firebase/init.js
@@ -0,0 +1,23 @@
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.8.2/firebase-app.js"></script>
+
+//<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.8.2/firebase-analytics.js"></script>
+
+<script>
+ 
+  var firebaseConfig = {
+    apiKey: "AIzaSyDI53lx0xAPoCOsRSjO2Q_CRGk2Jm0kXiM",
+    authDomain: "api-hot-brew.firebaseapp.com",
+    databaseURL: "https://api-hot-brew.firebaseio.com",
+    projectId: "api-hot-brew",
+    storageBucket: "api-hot-brew.appspot.com",
+    messagingSenderId: "1090628766284",
+    appId: "1:1090628766284:web:32d3dff705b9a5ff80c2e9",
+    measurementId: "G-4VXFBV4GY7"
+  };
+ 
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const firebase = require('firebase');
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${3000}`));
