1aa9663fbba077e48d377986ed31ccc47b709824
Leah H <harrichl@gmail.com>
2020-02-22 10:53:11 -0500

    wip get allusers route

---
+++ firebase/firebase.utils.js
@@ -0,0 +1,41 @@
+import firebase from 'firebase/firebase-app'
+import 'firebase/firebase-firestore'
+import 'firebase/firebase-auth'
+ 
+  const firebaseConfig = {
+  apiKey: "AIzaSyDI53lx0xAPoCOsRSjO2Q_CRGk2Jm0kXiM",
+  authDomain: "api-hot-brew.firebaseapp.com",
+  databaseURL: "https://api-hot-brew.firebaseio.com",
+  projectId: "api-hot-brew",
+  storageBucket: "api-hot-brew.appspot.com",
+  messagingSenderId: "1090628766284",
+  appId: "1:1090628766284:web:32d3dff705b9a5ff80c2e9",
+  measurementId: "G-4VXFBV4GY7"
+  };
+
+// Get users data from firestore
+export const convertCollectionSnapshotUsersToMap = (collections) => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { displayName, email } = doc.data();
+
+    return {
+      routeName: encodeURI(email.toLowerCase()),
+      id: doc.id,
+      displayName,
+      email
+    }
+  })
+  console.log(transformedCollection)
+}
+ 
+firebase.initializeApp(firebaseConfig);
+  
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+// Firebase Firestore database
+// Initialize on cloud functions
+
+const admin = require('firebase-admin');
+
+let serviceAccount = './.env';
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+let db = admin.firestore();
+
+
 const express = require('express')
 require('dotenv').config()
 const firebase = require('firebase');
@@ -7,8 +21,40 @@ const app = express()
 app.use(cors())
  
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World 123...')
 })
+
+const convertCollectionSnapshotUsersToMap = (collections) => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { displayName, email } = doc.data();
+
+    return {
+      routeName: encodeURI(email.toLowerCase()),
+      id: doc.id,
+      displayName,
+      email
+    }
+  })
+}
+
+app.get('api/users', function (req, res) {
+  let users = [];
+  let usersRef = db.collection('users');
+  let allUsers = usersRef.get()
+    .then(snapshot => { 
+      snapshot.forEach(doc => {
+      console.log(doc.id, '=>', doc.data());
+      });
+
+      res.send(allUsers);
+      console.log(allUsers);
+      })
+  
+    .catch(err => {
+      console.log('Error getting documents', err);
+    });
+
+  })
  
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${3000}`));
+const port = 3001 ;
+app.listen(port, () => console.log(`Listening on port ${port}`));
