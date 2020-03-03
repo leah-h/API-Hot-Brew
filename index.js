ef1f625dcf124d09ac31e1e3a119b8466524be38
cmejones <1016cme@gmail.com>
2020-03-03 10:10:11 -0500

    updated api functions

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ admin.initializeApp({
 
 let db = admin.firestore();
 
-
 const express = require('express')
 const bodyParser = require('body-parser')
 require('dotenv').config()
@@ -20,19 +19,19 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors())
- 
+
 app.get('/', function (req, res) {
   res.send('Hello World 123...')
 })
 
 app.get('/api/users', async (req, res) => {
- 
+
   let usersRef = db.collection('users');
   let users = [];
   let allUsers = await usersRef.get()
   if (allUsers) {
     allUsers.forEach((doc) => {
-      users = {...users, [doc.id]: {...doc.data()} }
+      users.push(doc.data());
     })
   }
   res.send(users);
@@ -45,54 +44,176 @@ app.get('/api/products', async (req, res) => {
   let allProducts = await productsRef.get()
   if (allProducts) {
     allProducts.forEach((doc) => {
-      products = {...products, [doc.id]: {...doc.data() } }
-   
+      products.push({
+        productId: doc.id,
+        ...doc.data()
+        });
     })
   }
   res.send(products);
+  //console.log(products);
 })
 
 app.get('/api/products/:id', async (req, res) => {
-  let productRef = db.collection('products').doc(uid);
-  let getDoc = productRef.get()
-  .then(doc => {
-    if (!doc.exists) {
-      console.log('No such document!');
-    } else {
-      console.log('Document data:', doc.data());
-    }
-  })
+
+  let id = req.params.id
+
+  let productsRef = db.collection('products').doc(id);
+
+  let queryProduct = await productsRef.get()
+    .then(snapShot => {
+      res.send(snapShot.data());  
+     // console.log(snapShot.data()); 
+
+    })
+
   .catch(err => {
-    console.log('Error getting document', err);
+    console.log('Error getting documents', err);
   });
-
-//get all userse
-app.get('/api/users', async function (req, res) {
-  let usersRef = db.collection('users');
-  let users = {};
-  let allUsers = await usersRef.get()
-  if (allUsers) {
-    allUsers.forEach((doc) => {
-      users = {...users, [doc.id]: {...doc.data()} };
-    })
-  }
-  res.send(users);
   
+})
+
+
+// Add new item to products table
+app.post('/api/products/new', async (req, res) => {
+
+  let data = {
+      category: req.body.category, 
+      description: req.body.description,
+      flavorProfile: req.body.flavorProfile,
+      imageUrl: req.body.imageUrl,
+      name: req.body.name,
+      price: req.body.price,
+      product_id: req.body.product_id,
+      size: req.body.size,
+      type: req.body.type
+    }
+  
+    await db.collection('products').doc().set(data)
+      .then(() => {
+        res.status(200).send('item added to db')
+      })
+      .catch(error => {
+        res.status(404).send('item NOT added to db');
+      })
   })
 
-//get one user
-app.get('/api/users/:id', async function (req, res) {
-  let usersRef = db.collection('users');
-  let users = {};
-  let allUsers = await usersRef.get()
-  if (allUsers) {
-    allUsers.forEach((doc) => {
-      users = {...users, [doc.id]: {...doc.data()} };
+//GET products by category
+app.get('/api/products/category/:category', async (req, res) => {
+
+  let category = req.params.category;
+  
+  let itemsByCategory = [];
+  
+  await db.collection('products').where('category', '==', category).get()
+    .then(snapshot => {
+      snapshot.forEach(doc => {
+        itemsByCategory.push(doc.data())
+      })
+      res.send(itemsByCategory);
+    })
+
+    .catch(err => {
+      console.log('Error getting documents', err);
+    });
+
+})
+
+//DELETE product by id
+app.delete('/api/products/delete/:id', async (req, res) => {
+    
+    let id = req.params.id;
+    
+    await db.collection('products').doc(id).delete()
+      .then( () => {
+      res.status(200).send('Item successfully deleted.');
+  }) 
+      .catch(err => {
+    console.log('Error deleting document', err);
+      });
+    
+  })
+
+// Update an item by id
+// handle item.key must not be empty, handle on FE if key is not empty, 
+// send previous values as update values
+app.put('/api/products/update/:id', async (req, res) => {
+
+  let id = req.params.id;
+  let data = {
+    'category': req.body.category,
+    'description': req.body.description,
+    'flavorProfile': req.body.flavorProfile,
+    'imageUrl': req.body.imageUrl,
+    'name': req.body.name,
+    'type': req.body.type,
+    'price': req.body.price,
+    'product_id': req.body.product_id,
+    'size': req.body.size
+    }
+    
+    await db.collection('products').doc(id).update(data)
+      .then(() => {
+        res.status(200).send('Item successfully updated');
+      })
+      .catch(error => {
+        console.log(error);
+      })
+})  
+
+
+// Get all reviews
+app.get('/api/reviews', async (req, res) => {
+  let reviewsRef = db.collection('reviews');
+  let reviews = [];
+  let allReviews = await reviewsRef.get()
+  if (allReviews) {
+    allReviews.forEach((doc) => {
+      reviews.push({
+       // productId: doc.id,
+        ...doc.data()
+        });
     })
   }
-  res.send(users);
+  res.send(reviews);
+})
+
+// get reviews by product
+app.get('/api/reviews/:productId', async (req, res) => {
   
-  })
+  let productId = req.params.productId;
+  let reviewsRefByItem = db.collection('reviews').where('productId', '==', productId);
+  let reviewsByItem = [];
+  let allReviewsByItem = await reviewsRefByItem.get()
+  if (allReviewsByItem) {
+    allReviewsByItem.forEach((doc) => {
+      reviewsByItem.push({
+        productId: doc.id,
+        ...doc.data()
+        });
+    })
+  }
+  res.send(reviewsByItem);
+})
+
+// Add a review to an item
+app.post('/api/reviews/new', async (req, res) => {
+
+    let data = {
+      'userId': req.body.userId,
+      'productId': req.body.productId,
+      'review': req.body.review
+    }
+    
+    await db.collection('reviews').doc().set(data)
+      .then(() => {
+        res.status(200).send('Item review successfully submitted to db.');
+      })
+      .catch(error => {
+        console.log(error);
+      })
+}) 
+  
 
 const port = 3001 ;
 app.listen(port, () => console.log(`Listening on port ${port}`));
