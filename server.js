f888dfc46596f9259acdba10e5110de90dfb2c34
Leah H <harrichl@gmail.com>
2020-03-05 13:08:52 -0500

    revert/remove path.join

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cors())
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname + 'public')));
+app.use(express.static(__dirname + 'public'));
 
 
 app.get('/', function (req, res) {
