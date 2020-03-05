9052933f472c371b791e38e5ef32ec02d085bde5
Leah H <harrichl@gmail.com>
2020-03-05 16:45:08 -0500

    revised expressed static('public')

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(cors())
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(__dirname + 'public'));
+app.use(express.static('public'));
 
 
 app.get('/', function (req, res) {
