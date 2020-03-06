90570215c8478142aadf044ee240365921d6d41a
Leah H <harrichl@gmail.com>
2020-03-05 20:41:03 -0500

    add app.use(express.json())

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ app.use(cors())
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(express.static('public'));
+app.use(express.json());
+app.use(express.static('public'));
 
 
 app.get('/', function (req, res) {
